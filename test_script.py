3de2e11705a1bf20fe69a22481620a7bd5024ea0
George <worrall.george@gmail.com>
2018-07-14 18:10:19 +0100

    Modified test script.

--- test_script.py
+++ test_script.py
@@ -17,7 +17,7 @@ test_file = pathlib.Path('test_files/CB7_4SS_grid_1 _combi.shp')
 s1_winterwheat_field = gs_downloader.ProductQueryParams()
 
 start_season = datetime.date(2015, 9, 1)
-end_season = datetime.date(2015, 9, 2)
+end_season = datetime.date(2015, 9, 8)
 s1_winterwheat_field.acquisition_date_range(start_season, end_season)
 s1_winterwheat_field.product_details('S1', 'L1', 'GRD', 'IW', 'VV VH')
 s1_winterwheat_field.coords_from_file(str(test_file),  # MK44 2EE grid 3
@@ -26,7 +26,7 @@ s1_winterwheat_field.coords_from_file(str(test_file),  # MK44 2EE grid 3
 hub = gs_downloader.CopernicusHubConnection()
 totals1, s1products = hub.submit_query(s1_winterwheat_field)
 print(totals1)
-hub.download_products(s1products)
+#hub.download_products(s1products)
 
 # Test gs_localmanager
 gs_localmanager.check_integrity()
