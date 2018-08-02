d00c72ef387f1ba21ad5e6d0950c550b10f05b34
George <worrall.george@gmail.com>
2018-08-02 17:53:22 +0100

    Added support for specifying orbit direction in gs_downloader.

--- test_script.py
+++ test_script.py
@@ -18,7 +18,8 @@ end_season = datetime.date(2018, 5, 7)
 
 s1_winterwheat_field = gs_downloader.ProductQueryParams()
 s1_winterwheat_field.acquisition_date_range(start_season, end_season)
-s1_winterwheat_field.product_details('S1', 'L1', 'GRD', 'IW', 'VV VH')
+s1_winterwheat_field.product_details('S1', 'L1', 'GRD', 'IW', 'VV VH',
+                                    orbitdirection='Ascending')
 s1_winterwheat_field.coords_from_file(str(test_file))  # MK44 2EE grid 3
 s2_winterwheat_field = gs_downloader.ProductQueryParams()
 s2_winterwheat_field.acquisition_date_range(start_season, end_season)
@@ -30,10 +31,10 @@ totals1, s1products = hub.submit_query(s1_winterwheat_field)
 totals2, s2products = hub.submit_query(s2_winterwheat_field)
 
 gs_downloader.filter_overlaps(s2products, s2_winterwheat_field.ROI)
-hub.download_quicklooks(s2products)
-hub.download_products(s2products)
-hub.download_quicklooks(s1products)
-hub.download_products(s1products)
+#hub.download_quicklooks(s2products)
+#hub.download_products(s2products)
+#hub.download_quicklooks(s1products)
+#hub.download_products(s1products)
 
 # Test gs_localmanager
 gs_localmanager.check_integrity()
