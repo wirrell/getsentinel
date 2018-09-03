1616c372c01e69be206141346a49677d794a1150
George <worrall.george@gmail.com>
2018-09-03 17:32:26 +0100

    Updated autodocs config.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
